50319dc9415108ccedda7bee7c06c496d650e4b1
OooOoOn <aadrianswe@gmail.com>
2020-12-21 15:32:38 +0100

    Added login validation

---
+++ uppgift4/js/validation.js
@@ -0,0 +1,51 @@
+var username;
+
+function validation() {
+    var vCode = document.getElementById("inputCodeBrowser").value ? document.getElementById("inputCodeBrowser").value : document.getElementById("inputCodeMobile").value;
+    var vNumber = document.getElementById("inputNumberBrowser").value.substring(3) ? document.getElementById("inputNumberBrowser") : document.getElementById("inputMobile");
+    
+    if(vCode.length < 1 && vNumber.length < 1){
+        return "";
+    }
+    
+    switch (vNumber) {
+        case "+46766123456":
+            if(verifyCode(vCode)) {
+                setUsername(vNumber);
+                successfulLogin();
+            }
+            break;    
+        default:
+            alert('Incorrect number');
+            break;
+    }
+}
+
+function verifyCode(code) {
+    switch (code) {
+        case "1111":
+            return true;
+            break;    
+        default:
+            alert('Incorrect verification code');
+            break;
+    }
+}
+
+function setUsername(vNumber) {
+    switch (vNumber) {
+        case "+46766123456":
+            username = "Jon";
+            break;    
+        default:
+            username = "Anonymous";
+            break;
+    }
+}
+
+function successfulLogin() {
+
+//Redirecting to other page or webste code or you can set your own html page.
+window.location.replace('home.html');
+//window.open('home.html',"_blank")
+}
