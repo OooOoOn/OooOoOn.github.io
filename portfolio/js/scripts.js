20940cedfa92c760b59961a80d520aa78ed48473
OooOoOn <aadrianswe@gmail.com>
2023-04-05 19:37:43 +0200

    Remade skillset page.

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,3 +1,53 @@
+// Refs
+const wrapCta  = document.querySelector('#wrap-cta'),
+      btnCta   = document.querySelector('#cta'),
+      content  = document.querySelector('#content'),
+      btnClose = document.querySelector('#close');
+
+
+// Anime.js Commons Values for SVG Morph
+const common = {
+  targets: '.polymorph',
+  easing: 'easeOutQuad',
+  duration: 600,
+  loop: false
+};
+
+
+
+
+// Show content on click
+btnCta.addEventListener('click', () => {
+  // Elements apparence
+  wrapCta.classList.remove('active');
+  content.classList.add('active');
+  
+  // Morph SVG
+  anime({
+    ...common,
+    points: [
+      { value: '215,110 0,110 186,86 215,0' }
+    ],
+  });
+});
+
+
+// Hide content  
+btnClose.addEventListener('click', () => {
+  // Elements apparence
+  content.classList.remove('active');
+  wrapCta.classList.add('active');
+  
+  // Morph SVG
+  anime({
+    ...common,
+    points: [
+      { value: '215,110 0,110 0,0 215,0' }
+    ]
+  }); 
+});
+
+    
     //Animated bee
 
     const path = anime.path('#path');
@@ -311,6 +361,7 @@
           }
         });
       }
+
 
     const segments2 = document.getElementsByClassName('logo')
     Array.prototype.forEach.call(
