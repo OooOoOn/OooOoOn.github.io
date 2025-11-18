f397278eb5f5cfb1169699d23c0d57d740701e81
OooOoOn <aadrianswe@gmail.com>
2025-11-18 23:38:20 +0100

    Updated design

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -104,36 +104,36 @@ const common = {
 };
 
 
-// Show content on click
-btnCta.addEventListener('click', () => {
-  // Elements apparence
-  wrapCta.classList.remove('active');
-  content.classList.add('active');
+// // Show content on click
+// btnCta.addEventListener('click', () => {
+//   // Elements apparence
+//   wrapCta.classList.remove('active');
+//   content.classList.add('active');
   
-  // Morph SVG
-  anime({
-    ...common,
-    points: [
-      { value: '215,110 0,110 186,86 215,0' }
-    ],
-  });
-});
+//   // Morph SVG
+//   anime({
+//     ...common,
+//     points: [
+//       { value: '215,110 0,110 186,86 215,0' }
+//     ],
+//   });
+// });
 
 
-// Hide content  
-btnClose.addEventListener('click', () => {
-  // Elements apparence
-  content.classList.remove('active');
-  wrapCta.classList.add('active');
+// // Hide content  
+// btnClose.addEventListener('click', () => {
+//   // Elements apparence
+//   content.classList.remove('active');
+//   wrapCta.classList.add('active');
   
-  // Morph SVG
-  anime({
-    ...common,
-    points: [
-      { value: '215,110 0,110 0,0 215,0' }
-    ]
-  }); 
-});
+//   // Morph SVG
+//   anime({
+//     ...common,
+//     points: [
+//       { value: '215,110 0,110 0,0 215,0' }
+//     ]
+//   }); 
+// });
 
     
     //Animated bee
